6c8ba5892168b71b04a625a508dcc8405ec6ddf2
Olga0512 <ot@create.aau.dk>
2022-03-03 20:15:33 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssociationExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager companyA_Manager = new Manager();
+            string name = companyA_Manager.GetManagerName();
+            System.Console.WriteLine(name);
+            
+            SwipeCard companyA_Card = new SwipeCard();
+            System.Console.WriteLine(companyA_Card.MakerOfSwipecard());
+            
+            companyA_Manager.Logon(companyA_Card);
+        }
+    }
+
+    public class Manager
+    {
+        private string name;
+        public Manager() {
+            name = "Pera";
+        }
+
+        public Manager(string name)
+        { }
+
+        public string GetManagerName()
+        {  
+            return name;
+        }
+
+        public void Logon(SwipeCard obj)
+        {
+            obj.Swipe(this);
+        }
+    }
+
+    public class SwipeCard
+    {
+        public void Swipe(Manager obj)
+        {
+            // manager swipes card in the morning
+            System.Console.WriteLine("SwipeCard class: manager on Logon called Swipe ");
+        }
+
+        public string MakerOfSwipecard()
+        {
+            return "TheBestCardMakingCompany";
+        }
+    }
+
+}
